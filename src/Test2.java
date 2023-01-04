f03ff640307cd717a137eecd19b99d8b3250dff4
ELCOT <ELCOT@Lenovo>
2023-01-04 12:38:30 +0530

    Added checks

--- src/Test2.java
+++ src/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		String hungry ="yes";
 
-		if(hungry=="yes")
+		if(hungry=="yes"|| hungry=="Y")
 		{
 			System.out.println("eat something.....");
 		}
